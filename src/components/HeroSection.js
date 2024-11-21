8663b2cec425cb23b9113049ce51ad3d9dc3b1af
Ratpick <patrick.treloar@gmail.com>
2024-11-21 17:19:06 +0000

    Create HeroSection.js

---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+// src/components/HeroSection.js
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="hero">
+      <h1>Paradoxica Universica</h1>
+      <p>A Journey Through Consciousness and Paradox</p>
+    </div>
+  );
+};
+
+export default HeroSection;
