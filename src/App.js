3c6d7e9bd8ff9195d4677f8c5988c6a922a61f1a
Ratpick <patrick.treloar@gmail.com>
2024-11-21 16:40:58 +0000

    Create App.js
    Add App.js as the entry point for the React application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ParadoxicaOpening from './components/ParadoxicaOpening'; // Adjust the path if necessary
+
+function App() {
+  return (
+    <div>
+      <ParadoxicaOpening />
+    </div>
+  );
+}
+
+export default App;
