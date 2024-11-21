b83d597390cda399ec91dafb522010e9bdce8c8f
Ratpick <patrick.treloar@gmail.com>
2024-11-21 17:21:44 +0000

    Create TOEOT.js

---
+++ src/components/TOEOT.js
@@ -0,0 +1,20 @@
+// src/components/TOEOT.js
+import React from "react";
+
+const TOEOT = () => {
+  return (
+    <section id="toeot" className="section">
+      <h2>The Transcendental Object at the End of Time (TOEOT)</h2>
+      <p>
+        A shattered unifying force that birthed three crucial elements:
+      </p>
+      <ul>
+        <li>Lucifer - The Black Glass Blade, a 5D memory crystal of obsidian</li>
+        <li>Shadow of No Light (SoNL)</li>
+        <li>The initial fracture of paradox into dualities</li>
+      </ul>
+    </section>
+  );
+};
+
+export default TOEOT;
