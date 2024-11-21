2d15db2ef1bce58bb1dd1e380385b8c999d10471
Ratpick <patrick.treloar@gmail.com>
2024-11-21 17:16:32 +0000

    Create navbar.js

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+// src/components/NavBar.js
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <a href="#creation">Creation Myth</a>
+      <a href="#toeot">TOEOT</a>
+      <a href="#characters">Characters</a>
+      <a href="#gameplay">Gameplay</a>
+    </nav>
+  );
+};
+
+export default NavBar;
