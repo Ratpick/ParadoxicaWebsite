ef34cd973e21bc8836510645c8d84da402e218da
Ratpick <patrick.treloar@gmail.com>
2024-11-21 17:25:41 +0000

    Create GameplayEvolution.js

---
+++ src/components/GameplayEvolution.js
@@ -0,0 +1,42 @@
+// src/components/GameplayEvolution.js
+import React from "react";
+
+const GameplayEvolution = () => {
+  return (
+    <section id="gameplay" className="section">
+      <h2>Game Theory Evolution</h2>
+      <table>
+        <thead>
+          <tr>
+           </th>
+            <th>Significance</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Basic</td>
+            <td>Draughts</td>
+            <td>Introduction to duality</td>
+          </tr>
+          <tr>
+            <td>Intermediate</td>
+            <td>Chess</td>
+            <td>Mastery of finite systems</td>
+          </tr>
+          <tr>
+            <td>Advanced</td>
+            <td>Go</td>
+            <td>Balance and territory</td>
+          </tr>
+          <tr>
+            <td>Transcendent</td>
+            <td>Infinite Weiqi</td>
+            <td>Paradox integration guided by Thunder Perfect Mind</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default GameplayEvolution;
